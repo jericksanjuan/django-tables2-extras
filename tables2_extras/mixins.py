0a47f93f7fc3e41b820349564c67ea8de1146523
Jerick Don San Juan <jerick@icannhas.com>
2014-05-16 13:32:29 +0800

    Added table option order_by and convert_to_list (for tables with non modelfield)

--- tables2_extras/mixins.py
+++ tables2_extras/mixins.py
@@ -38,6 +38,7 @@ class ModelSingleTableMixin(SingleTableMixin):
     empty_text = 'Nothing to display'
     orderable = True
     prefix = ''
+    order_by = None
 
     def get_table_class(self):
         if self.table_class:
@@ -47,7 +48,7 @@ class ModelSingleTableMixin(SingleTableMixin):
             return model_table_factory(
                 self.model, self.table_parent_class, table_fields=self.table_fields, table_exclude=self.table_exclude, table_template=self.table_template,
                 table_show_link=self.table_show_link, table_row_url=self.table_row_url, table_delete_url=self.table_delete_url, empty_text=self.empty_text,
-                paginate_by=self.paginate_by, orderable=self.orderable, prefix=self.prefix)
+                paginate_by=self.paginate_by, orderable=self.orderable, prefix=self.prefix, order_by=self.order_by)
 
         msg = "'%s' must either define 'table_class' or both 'model' and " \
             "'fields', or override 'get_table_class()'"
@@ -81,16 +82,38 @@ class TableHelperMixin(object):
         if not self.tconfig:
             self.config = RequestConfig(self.request)
 
+        convert_to_list = kwargs.get('convert_to_list')
         if not table_class:
             if with_report:
                 table_class = create_report_table(qs.model, **kwargs)
             else:
                 table_class = create_model_table(qs.model, **kwargs)
 
-        table = table_class(qs)
+        if convert_to_list:
+            table_fields = kwargs.get('table_fields', [])
+            if table_fields:
+                qs_list = self.convert_qs_to_list(qs, table_fields)
+            else:
+                qs_list = qs.values()
+            qs_list = list(qs_list)
+            table = table_class(qs_list)
+        else:
+            table = table_class(qs)
         self.config.configure(table)
         self.mtables = self.mtables + 1
         context[context_name or self.get_new_table_context_name()] = table
+
+    def convert_qs_to_list(self, qs, table_fields):
+        qs_list = []
+        for obj in qs:
+            obj_dict = {}
+            for f in table_fields:
+                x = getattr(obj, f)
+                if hasattr(x, '__call__'):
+                    x = x()
+                obj_dict[f] = x
+            qs_list.append(obj_dict)
+        return qs_list
 
 
 class DisplaySearchMixin(object):
--- tables2_extras/utils.py
+++ tables2_extras/utils.py
@@ -13,7 +13,7 @@ _PAGINATE_BY = 12
 
 def model_table_factory(
     model, table_parent_class, table_fields=None, table_exclude=None, table_template=TABLE_TEMPLATE,
-    paginate_by=_PAGINATE_BY, empty_text='Nothing to display', orderable=True, prefix='',
+    paginate_by=_PAGINATE_BY, empty_text='Nothing to display', orderable=True, prefix='', order_by='', convert_to_list=False,
         table_row_url=None, table_update_url=None, table_delete_url=None, table_show_link=True, table_pagination=None):
 
     name = "{}Table".format(model.__name__)
@@ -26,11 +26,16 @@ def model_table_factory(
         meta['exclude'] = table_exclude
     if paginate_by:
         meta['per_page'] = paginate_by
+    if order_by:
+        meta['order_by'] = order_by
     Meta = type(TableOptions)(str('Meta'), (TableOptions,), meta)
 
-    attrs = dict(
-        table_row_url=table_row_url, table_delete_url=table_delete_url, table_show_link=table_show_link,
-        table_update_url=table_update_url, Meta=Meta)
+    if convert_to_list:
+        attrs = dict(Meta=Meta)
+    else:
+        attrs = dict(
+            table_row_url=table_row_url, table_delete_url=table_delete_url, table_show_link=table_show_link,
+            table_update_url=table_update_url, Meta=Meta)
     table_class = type(name, (table_parent_class,), attrs)
 
     return table_class
